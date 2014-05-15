dad387f81a794db949d44fe6e8596b98acd97e9f
paredero <garcia.paredero@gmail.com>
2014-05-16 00:32:09 +0200

    No terminales

---
+++ src/compiler/syntax/nonTerminal/ListaParametrosEjecucion.java
@@ -0,0 +1,33 @@
+package compiler.syntax.nonTerminal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.uned.lsi.compiler.semantic.type.TypeIF;
+
+public class ListaParametrosEjecucion {
+	private List<TypeIF> parameterTypes;
+	
+	public ListaParametrosEjecucion() {
+		parameterTypes = new ArrayList<TypeIF>();
+	}
+
+	/**
+	 * @return the parameterTypes
+	 */
+	public List<TypeIF> getParameterTypes() {
+		return parameterTypes;
+	}
+
+	/**
+	 * @param parameterTypes the parameterTypes to set
+	 */
+	public void setParameterTypes(List<TypeIF> parameterTypes) {
+		this.parameterTypes = parameterTypes;
+	}
+	
+	public void addParameterType(TypeIF type) {
+		this.parameterTypes.add(type);
+	}
+
+}
---
+++ src/compiler/syntax/nonTerminal/Referencia.java
@@ -0,0 +1,69 @@
+package compiler.syntax.nonTerminal;
+
+import es.uned.lsi.compiler.semantic.type.TypeIF;
+
+public class Referencia extends NonTerminal {
+
+	private TypeIF type;
+	private String lexema;
+	private int line;
+	
+	public Referencia() {
+	}
+
+	/**
+	 * @return the type
+	 */
+	public TypeIF getType() {
+		return type;
+	}
+
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(TypeIF type) {
+		this.type = type;
+	}
+
+	/**
+	 * @return the lexema
+	 */
+	public String getLexema() {
+		return lexema;
+	}
+
+	/**
+	 * @param lexema the lexema to set
+	 */
+	public void setLexema(String lexema) {
+		this.lexema = lexema;
+	}
+
+	/**
+	 * @return the line
+	 */
+	public int getLine() {
+		return line;
+	}
+
+	/**
+	 * @param line the line to set
+	 */
+	public void setLine(int line) {
+		this.line = line;
+	}
+
+	/**
+	 * @param type
+	 * @param lexema
+	 * @param line
+	 */
+	public Referencia(TypeIF type, String lexema, int line) {
+		super();
+		this.type = type;
+		this.lexema = lexema;
+		this.line = line;
+	}
+
+	
+}
