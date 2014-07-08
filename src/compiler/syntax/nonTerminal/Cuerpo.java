2c114d4591d0b13762645fe33768302b894e0b22
paredero <garcia.paredero@gmail.com>
2014-07-08 21:25:03 +0200

    codigo intermedio + final

---
+++ src/compiler/syntax/nonTerminal/Cuerpo.java
@@ -0,0 +1,21 @@
+package compiler.syntax.nonTerminal;
+
+public class Cuerpo extends NonTerminal {
+	String idProcedimiento;
+
+	/**
+	 * @return the idProcedimiento
+	 */
+	public String getIdProcedimiento() {
+		return idProcedimiento;
+	}
+
+	/**
+	 * @param idProcedimiento the idProcedimiento to set
+	 */
+	public void setIdProcedimiento(String idProcedimiento) {
+		this.idProcedimiento = idProcedimiento;
+	}
+	
+	
+}
